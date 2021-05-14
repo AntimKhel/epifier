1582eb683a24cef1ed2d5e18ed68803cbe5f6e3c
Ritwik <raj.ritwik96@gmail.com>
2021-05-14 16:31:18 +0530

    Added date logic

--- app/src/main/java/com/example/epifier/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/epifier/viewmodel/MainViewModel.kt
@@ -5,6 +5,13 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
+import java.lang.Exception
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -33,6 +40,15 @@ class MainViewModel @Inject constructor() : ViewModel() {
 
     val isButtonEnabled: Flow<Boolean> = combine(_pan, _day, _month, _year) { pan, day, month, year ->
         val regex = "[A-Z]{5}[0-9]{4}[A-Z]".toRegex()
-        return@combine regex.containsMatchIn(pan)
+        return@combine regex.containsMatchIn(pan) and validateDate(day, month, year)
+    }
+
+    private fun validateDate(day: String, month: String, year: String): Boolean {
+        return try {
+            LocalDate.parse("$year$month$day", DateTimeFormatter.BASIC_ISO_DATE)
+            true
+        } catch (e: DateTimeParseException) {
+            return false
+        }
     }
 }
