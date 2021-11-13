4a12057dd2681139e7d3d6685c5ba42608034bdb
IAmA787 <raj.ritwik96@gmail.com>
2021-11-13 22:56:55 +0530

    slight refactor

--- app/src/main/java/com/example/epifier/extention/ViewExtention.kt
+++ app/src/main/java/com/example/epifier/extention/ViewExtention.kt
@@ -9,11 +9,9 @@ import com.google.android.material.textfield.TextInputEditText
 
 fun TextInputEditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
     this.addTextChangedListener(object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        }
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
 
         override fun afterTextChanged(editable: Editable?) {
             afterTextChanged.invoke(editable.toString())
